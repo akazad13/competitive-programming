df3abeafa6f2d2acaecd6fc5e030af82c46eb997
Md. Abul Kalam <akazad.cse13@gmail.com>
2024-05-21 13:43:33 +0600

    Add New Solution for LeetCode

---
+++ Online Judges/LeetCode/1295 - Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+     int findNumbers(vector<int> &nums) {
+         int count=0;
+         for(int num : nums)
+         {
+             if(to_string(num).size()%2==0)
+             {
+                 count++;
+             }
+         }
+         return count;
+     }
+};
